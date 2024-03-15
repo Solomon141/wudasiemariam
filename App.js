604d990b3095add8fa3c4802c9f0a738b7b96d21
Solomon141 <weymit2001@gmail.com>
2024-03-15 11:44:45 +0300

    Updates on the tselot

--- App.js
+++ App.js
@@ -1,19 +1,6 @@
 import React from "react";
-
 import WStackNav from "./src/common/navigation/WStackNav";
-import store from "./src/common/redux/Store";
-
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/lib/integration/react";
-import persistStore from "redux-persist/es/persistStore";
-let persistor = persistStore(store);
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <WStackNav />
-      </PersistGate>
-    </Provider>
-  );
+  return <WStackNav />;
 }
